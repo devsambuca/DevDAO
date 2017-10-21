30d38cf5308bbe376e18f7807f8cc3eead0be9d0
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-10-21 18:03:11 +0300

    Added method save

--- src/net/devsambuca/DeveloperApp.java
+++ src/net/devsambuca/DeveloperApp.java
@@ -8,10 +8,16 @@ public class DeveloperApp {
     public static void main(String[] args) {
 
         DeveloperDAO d1 = new DeveloperDAO();
+       Developer developer = new Developer(7L, "Nikolay", "Petunin", "Java Developer", 40000.00);
+       d1.countDev();
+
+        d1.save(developer);
 
         d1.getAll();
 
         d1.getById(1l);
+
+        d1.update(developer);
 
 
     }
--- src/net/devsambuca/DeveloperDAO.java
+++ src/net/devsambuca/DeveloperDAO.java
@@ -7,6 +7,51 @@ import java.util.*;
  * @author Fominykh Vladimir
  */
 public class DeveloperDAO {
+    Developer [] devArr;
+
+    public Developer[] countDev() {
+
+        int lineCount = 0;
+        try {
+            // find the file with the developer date
+            File devFile = new File("test.txt");
+
+            Scanner devScanner = new Scanner(devFile);
+
+            while (devScanner.hasNext()) {
+                Developer developer = new Developer();
+                String nextLine = devScanner.nextLine();
+                String[] devData = nextLine.split(",");
+                lineCount++;
+                developer.setId((Long.parseLong(devData[0])));
+                developer.setFirstName(devData[1]);
+                developer.setLastName(devData[2]);
+                developer.setPosition(devData[3]);
+                developer.setSalary(Double.parseDouble(devData[4]));
+
+                Developer [] devArr = new Developer[lineCount];
+                for(int i = 0; i < devArr.length; i++)
+                    devArr[i] = developer;
+                    System.out.println(developer);
+            }
+            System.out.println();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new Developer[0];
+    }
+
+    public Developer save(Developer developer){
+        developer.getId();
+        developer.getFirstName();
+        developer.getLastName();
+        developer.getPosition();
+        developer.getSalary();
+
+        System.out.println(developer.toString());
+        return developer;
+    }
 
     public Developer getAll() {
         try {
@@ -19,24 +64,25 @@ public class DeveloperDAO {
                 Developer developer = new Developer();
                 String nextLine = devScanner.nextLine();
                 String[] devData = nextLine.split(",");
+
                 developer.setId((Long.parseLong(devData[0])));
                 developer.setFirstName(devData[1]);
                 developer.setLastName(devData[2]);
                 developer.setPosition(devData[3]);
                 developer.setSalary(Double.parseDouble(devData[4]));
                 System.out.println(developer.toString());
-            }
 
+            }
+            System.out.println();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return null;
     }
+
+
     public Developer deletebyId(Long id){
-        Developer developer = new Developer();
-        if(id == developer.getId())
-
-
+       Developer[]devArr = countDev();
     }
 
     public Developer getById(Long id) {
@@ -59,6 +105,7 @@ public class DeveloperDAO {
                 if (id != null && id == developer.getId()){
                     System.out.println(developer.toString());
                 }
+                return developer;
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
@@ -66,23 +113,20 @@ public class DeveloperDAO {
         return null;
     }
 
+
     public void update(Developer developer) {
-        System.out.println(developer.toString());
+
         Writer writer = null;
         String str = developer.toString();
         try {
-            writer = new FileWriter("file.txt",true);
-            writer.write(str +'\n');
+            writer = new FileWriter("test.txt",true);
+            writer.write(str);
 
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
-
-
 }
 
 
